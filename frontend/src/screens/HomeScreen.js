d529a792310be95421689c862bfc1d7443c64e33
LillyVernon <littletomoya@yahoo.com>
2022-07-28 19:21:51 -0500

    Hooks, Axios and UseEffect
    the products were fetched from the backend using axios instead of using data.js from the frontend

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,14 +1,29 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
-import data from '../data'
+//import data from '../data'
+import axios from 'axios'
 
 function HomeScreen() {
+  const [products, setProducts]=useState([]);
+
+  //useEffect to get data after rendering
+  useEffect(()=>{
+    const fetchData =async()=>{
+      //axios fetch data from backend
+      const result= await axios.get('/api/products');
+
+      //set the state of the products with the data recieved from backend
+      setProducts(result.data)
+    };
+
+    fetchData()
+  }, []);
   return (
     <div>
          <h1> Featured Products</h1>
         <div className='products'>
         {
-        data.products.map(product=>(
+        products.map(product=>(
         <div className='product' key={product.slug}> 
         <Link   to={`/product/${product.slug}`}>
           <img src={product.image} alt={product.name}></img>
